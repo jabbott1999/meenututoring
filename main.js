607a8b1702ac10895e6dbad720fe25bc0e8b35fa
psyja4 <psyja4@exmail.nottingham.ac.uk>
2020-06-24 15:13:45 +0100

    menu bar done

--- main.js
+++ main.js
@@ -6,7 +6,26 @@ $(document).ready(function () {
 
 
 
+    scrollCheck();
 
+    $(document).on("scroll", function () {
 
-
+        scrollCheck();
+    });
 });
+
+
+function scrollCheck() {
+    var pageTop = $(document).scrollTop();
+
+    if (pageTop > 0) {
+        $("#small-logo").css("display", "block");
+        $("#big-logo").css("display", "none");
+        $("#nav-container").css("height", "50px");
+    } else if (pageTop == 0) {
+
+        $("#small-logo").css("display", "none");
+        $("#big-logo").css("display", "block");
+        $("#nav-container").css("height", "150px");
+    }
+}
